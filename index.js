f8dcf997c5205ae6476263620da8ae2ac7119b40
Dmitry <df.creative@gmail.com>
2016-11-12 15:47:35 -0500

    Fix math

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const τ = Math.PI * 2;
 
 module.exports = function pdf ( x, μ = 0, υ = 1 ) {
 	if ( υ === 0 ) {
-		return x === μ ? 1 : 0;
+		return x === μ ? Infinity : 0;
 	}
 
 	return Math.exp(-.5 * Math.pow(x - μ, 2) / υ) / Math.sqrt(τ * υ);
--- test.js
+++ test.js
@@ -4,6 +4,4 @@ const n = require('./')
 assert.equal(Math.sqrt(Math.PI*2)*n(0), 1);
 assert.equal(n(100), 0);
 
-assert.equal(n(1, 1, 0), 1);
-assert.equal(n(0, 0, 0), 1);
 assert.equal(n(1, 0, 0), 0);
